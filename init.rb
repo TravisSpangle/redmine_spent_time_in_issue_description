bf91425942315dc78bd8d321c7bc083486b80da9
travis <spangltk@gmail.com>
2014-04-28 15:21:17 -0700

    Completed view of entries on Issue Description
    Added Horizontal Rule to separate content. Only displays the last five
    entries. Links to rest of entries if more than five.

--- init.rb
+++ init.rb
@@ -7,6 +7,6 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
   author 'Travis Spangle'
   description 'This will feed the spent time into the description of the issue.'
-  version '0.5'
+  version '0.8'
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
 end
