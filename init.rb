d1b6bfde8135f04a9f0cb626b3b19962158be9ca
travis <spangltk@gmail.com>
2014-08-14 13:34:03 -0700

    Updated version
    1.1 is Making use of Rails i18 to translate the plugin for other
    languages.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
   author 'Travis Spangle'
   description 'This will feed the spent time into the description of the issue.'
-  version '1'
+  version '1.1'
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
   settings :default => { 'spent_time_max_display'  => 5 }, :partial => 'settings/spent_time_settings'
 end
