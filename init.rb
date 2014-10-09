a5396cbd677b9552843cc15f745fda253a43b22f
travis <spangltk@gmail.com>
2014-10-09 16:13:46 -0700

    closes #4
    I like the values better in the multi select list.

--- init.rb
+++ init.rb
@@ -7,7 +7,10 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
   author 'Travis Spangle'
   description 'This will feed the spent time into the description of the issue.'
-  version '1'
+  version '1.3'
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
-  settings( :default => { 'spent_time_max_display'  => true, 'show_user' => true, 'show_hours' => true, 'show_comments' => true }, :partial => 'settings/spent_time_settings' )
+  settings( :default => { 'spent_time_max_display'  => 5,
+                          'display_columns' => ["spentOn", "user", "hours", "comments"]
+                        },
+                        :partial => 'settings/spent_time_settings' )
 end
