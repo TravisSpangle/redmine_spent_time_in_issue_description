33d006a9935ac64fb5775e446c5d32c169990210
travis <spangltk@gmail.com>
2014-10-27 10:52:47 -0700

    Initial commit for #4
    Settings allow you to configure where the spent time report appears.
    Need to bring customizations into floating report. It’s not using
    localized language or custom columns.

--- init.rb
+++ init.rb
@@ -11,7 +11,8 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
   settings( :default => { 'spent_time_max_display'  => 5,
                           'display_columns' => ["spentOn", "user", "hours", "comments"],
-                          'time_format' => 'decimal'
+                          'time_format' => 'decimal',
+                          'report_location' => 'ticket_body'
                         },
                         :partial => 'settings/spent_time_settings' )
 end
--- lib/issue_description_with_spent_time_hook_listener.rb
+++ lib/issue_description_with_spent_time_hook_listener.rb
@@ -1,4 +1,5 @@
 class IssueDescriptionWithSpentTimeHookListener < Redmine::Hook::ViewListener
   render_on :view_issues_show_description_bottom, :partial => "spent_time/report"
+  render_on :view_layouts_base_html_head, :partial => "spent_time/javascript_report"
 end
 
