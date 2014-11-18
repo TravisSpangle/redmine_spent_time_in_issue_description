71460ea7faacc79504a038cf3f731b347b1eff4a
travis <spangltk@gmail.com>
2014-11-18 11:29:56 -0800

    Extended Show View to simplify process. Closes #4
    Extending show removed JavaScript dependancy. This limits support for
    the plugin for Redmine 2.2 to current.

--- init.rb
+++ init.rb
@@ -1,7 +1,5 @@
 require 'redmine'
 
-require_dependency 'issue_description_with_spent_time_hook_listener'
-
 Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
   author 'Travis Spangle'
