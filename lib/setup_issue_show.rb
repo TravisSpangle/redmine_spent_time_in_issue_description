937cc646d0d452e5b97476d031df0d2ab99bd52d
travis <spangltk@gmail.com>
2015-02-19 13:20:05 -0800

    Corrections after code review with rubocop.
    Corrected redundant variable names. Removed useless variable
    assignment.

--- lib/setup_issue_show.rb
+++ lib/setup_issue_show.rb
@@ -2,28 +2,29 @@ require 'redmine'
 require 'fileutils'
 require 'pathname_redmine_version'
 
+# Identifies correct version of issues/VERSION.show.html.erb and
+# copies it to issues/show.html.erb
 class SetupIssueShow
   attr_accessor :issues_directory
   attr_accessor :current_version
 
   def initialize
-    @issues_directory =  Pathname.new( Redmine::Plugin.registered_plugins[:redmine_spent_time_in_issue_description].directory ).join("app","views","issues")
-    @current_version = [ Redmine::VERSION::MAJOR, Redmine::VERSION::MINOR, Redmine::VERSION::TINY ].join('.')
+    plugin_directory = Redmine::Plugin.registered_plugins[:redmine_spent_time_in_issue_description].directory
+    @issues_directory =  Pathname.new(plugin_directory).join('app', 'views', 'issues')
+    @current_version = [Redmine::VERSION::MAJOR, Redmine::VERSION::MINOR, Redmine::VERSION::TINY].join('.')
   end
 
   def show_file
-    possible_files = @issues_directory.children(false).select{ |files| files.to_s.index(/\d.*show/) }
-    file = possible_files.sort{|x,y| y <=> x }.find do |file|
+    possible_files = @issues_directory.children(false).select { |files| files.to_s.index(/\d.*show/) }
+    show_file = possible_files.sort { |x, y| y <=> x }.find do |file|
       version = file.to_s[0..4]
       version == @current_version || version < @current_version
     end
 
-    show_file = @issues_directory.join file unless file.nil?
-
+    @issues_directory.join show_file unless show_file.nil?
   end
 
   def replace
     FileUtils.copy_file show_file.to_s, @issues_directory.join('show.html.erb').to_s
   end
-
 end
