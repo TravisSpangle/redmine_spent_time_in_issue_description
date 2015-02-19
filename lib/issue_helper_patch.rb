58296827673177bda46d185e027cba9eb2efc809
travis <spangltk@gmail.com>
2015-02-19 13:23:38 -0800

    corrections after rubocop
    corrected method naming conventions  and cleaned up syntax.

--- lib/issue_helper_patch.rb
+++ lib/issue_helper_patch.rb
@@ -1,61 +1,66 @@
+# Helper Patch to be include in Issue Helper
 module IssuesHelperPatch
-
   def self.included(base) # :nodoc:
     base.send(:include, InstanceMethods)
   end
 
+  # Helper provides shorter conditional calls and humanized_time format
   module InstanceMethods
-
     def humanized_time(entry_hours)
       hours = entry_hours.to_i
       minutes = entry_hours * 60
-      minutes = (minutes - hours * 60 ).to_i
+      minutes = (minutes - hours * 60).to_i
 
-      time_spent = case
-      when hours == 1
-        t('plugin_spent_time_in_issue.datetime.hours.one')
-      when hours > 1
-        t('plugin_spent_time_in_issue.datetime.hours.other', hours: hours)
-      else
-        ""
-      end
-
-      if minutes == 1
-        time_spent << " " << t('plugin_spent_time_in_issue.datetime.minutes.one')
-      elsif minutes > 1
-        time_spent << " " << t('plugin_spent_time_in_issue.datetime.minutes.other', minutes: minutes)
-      else
-        time_spent
-      end
+      time_spent = humanized_hour(hours)
+      time_spent << ' ' << humanized_minute(minutes) unless minutes < 1
     end
+  end
 
+  def humanized_hour(hours)
+    if hours == 1
+      t('plugin_spent_time_in_issue.datetime.hours.one')
+    elsif hours > 1
+      t('plugin_spent_time_in_issue.datetime.hours.other', hours: hours)
+    else
+      ''
+    end
+  end
+
+  def humanized_minute(minutes)
+    if minutes == 1
+      ' ' << t('plugin_spent_time_in_issue.datetime.minutes.one')
+    elsif minutes > 1
+      ' ' << t('plugin_spent_time_in_issue.datetime.minutes.other', minutes: minutes)
+    else
+      ''
+    end
   end
 
   def report_time_spent(entry)
-    if Setting.plugin_redmine_spent_time_in_issue_description['time_format'].eql? "human"
-      time_spent = humanized_time( entry.hours )
+    if Setting.plugin_redmine_spent_time_in_issue_description['time_format'].eql? 'human'
+      humanized_time(entry.hours)
     else
-      time_spent = entry.hours
+      entry.hours
     end
   end
 
-  def is_report_time_shown( entry )
+  def report_time_shown?
     Setting.plugin_redmine_spent_time_in_issue_description['display_columns'].include? 'hours'
   end
 
-  def is_spent_on_shown( entry )
+  def spent_on_shown?
     Setting.plugin_redmine_spent_time_in_issue_description['display_columns'].include? 'spentOn'
   end
 
-  def is_user_shown( entry )
+  def user_shown?
     Setting.plugin_redmine_spent_time_in_issue_description['display_columns'].include? 'user'
   end
 
-  def is_comment_shown( entry )
+  def comment_shown?
     Setting.plugin_redmine_spent_time_in_issue_description['display_columns'].include? 'comments'
   end
 
-  def is_activity_shown( entry )
+  def activity_shown?
     Setting.plugin_redmine_spent_time_in_issue_description['display_columns'].include? 'activity'
   end
 
