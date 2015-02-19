7edc3cb8e79c9375b25e7444574e899019c7fd35
travis <spangltk@gmail.com>
2015-02-19 13:29:17 -0800

    update to version and syntax
    version 2.5 includes the Issue Show setup procedure.

--- init.rb
+++ init.rb
@@ -6,14 +6,14 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
   author 'Travis Spangle'
   description 'This will feed the spent time into the description of the issue.'
-  version '2'
+  version '2.5'
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
-  settings( :default => { 'spent_time_max_display'  => 5,
-                          'display_columns' => ["spentOn", "user", "hours", "comments"],
-                          'time_format' => 'decimal',
-                          'report_location' => 'ticket_body'
-                        },
-                          :partial => 'settings/spent_time_settings' )
+  settings(default: {  'spent_time_max_display'  => 5,
+                       'display_columns' => %w('spentOn', 'user', 'hours', 'comments'),
+                       'time_format' => 'decimal',
+                       'report_location' => 'ticket_body'
+                    },
+           partial: 'settings/spent_time_settings')
 
   ActionDispatch::Callbacks.to_prepare do
     SetupIssueShow.new.replace
