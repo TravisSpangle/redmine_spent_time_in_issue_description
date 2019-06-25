70490c779dcba2e7f3e1bbb959c738e3ee6fde20
tbird1965 <20564407+tbird1965@users.noreply.github.com>
2019-06-25 14:10:25 -0400

    Fix for Redmine 4.0.4
    Fix for Rails >=5.1.  Tested on 4.0.4

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'setup_issue_show'
 
 Redmine::Plugin.register :redmine_spent_time_in_issue_description do
   name 'Issue Description with Spent Time'
-  author 'Travis Spangle'
+  author 'Travis Spangle, @dsatsura, @tbird1965'
   description 'This will feed the spent time into the description of the issue.'
-  version '2.8.0'
+  version '2.8.2'
   url 'https://github.com/TravisSpangle/redmine_spent_time_in_issue_description'
   settings(default: {  'spent_time_max_display'  => 5,
                        'display_columns' => %w(spentOn user hours comments),
@@ -14,7 +14,9 @@ Redmine::Plugin.register :redmine_spent_time_in_issue_description do
                     },
            partial: 'settings/spent_time_in_issue_description_settings')
 
-  ActionDispatch::Callbacks.to_prepare do
+  #to_prepare removed from Rails 5.1
+  #ActionDispatch::Callbacks.to_prepare do
+  ActiveSupport::Reloader.to_prepare do
     SetupIssueShow.new.replace
   end
   Rails.application.config.after_initialize do
